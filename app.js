818c4bab2a84232137748e6ca3d5817d75fad893
hvoncannon <hayden.voncannon@gmail.com>
2019-01-23 20:47:29 -0800

    added images, fixed modal js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-
-// const element = document.getElementById("img1"); 
-
-// element.addEventListener("click", myFunction);
-
+$("#img1").on("click", function() {
+  $("#exampleModalLongTitle").html("Project 1");
+  $(".modal-body").html("description of project 1");
+  $(".modal-image").html('<img class="img-thumbnail" src="http://cohenwoodworking.com/wp-content/uploads/2016/09/image-placeholder-500x500.jpg">');
+  $("#exampleModalCenter").modal("show");
+})
