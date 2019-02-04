c2cea31bd2c71f6f7a2cbacd088bf6f6ff532692
hvoncannon <hayden.voncannon@gmail.com>
2019-02-03 16:06:46 -0800

    added links in contact table and styled table

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ $("#img1").on("click", function() {
   $(".modal-image").html('<img class="img-thumbnail" src="http://cohenwoodworking.com/wp-content/uploads/2016/09/image-placeholder-500x500.jpg">');
   $("#exampleModalCenter").modal("show");
 })
+
+ $("#link1").on("click", function () {
+    const copyText = ($(this)[0]);
+    console.log(copyText);
+    // copyText.select();
+    alert(copyText);
+    document.execCommand("copy");
+  })
